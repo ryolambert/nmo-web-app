f8fbcba3451613a0f808607530fbad6d8f7916ee
James R. Lambert <ryolambert@gmail.com>
2018-12-24 09:16:18 -0700

    Fix(import): fixed cookieSession import so it's works now.

--- packages/backend/src/middleware/index.js
+++ packages/backend/src/middleware/index.js
@@ -1,20 +1,17 @@
 const passport = require('passport');
 const { Router } = require('express');
 const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie');
+const cookieSession = require('cookie-session');
 
 const middleWare = Router();
 middleWare.use(
   cookieSession({
     keys: [process.env.APP_SECRET],
-    maxAge: 1000 * 60 * 60 * 24 * 7, // * Cookie age set to 7 days
-  }),
+    maxAge: 1000 * 60 * 60 * 24 * 7 // * Cookie age set to 7 days
+  })
 );
 
 middleWare.use(passport.initialize());
 middleWare.use(passport.session());
 
 module.exports = middleWare;
-
-
-
